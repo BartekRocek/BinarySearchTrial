73f2a8f7d9946b5d949d56e9de3fc7a1a1103b5d
brocl <brocl@DESKTOP-NJMFVEB>
2019-12-30 23:13:13 +0100

    A sample code selecting random numbers, later on just one will be selected for the code to guess it

--- BinarySearchTrial/src/Binary_Search.java
+++ BinarySearchTrial/src/Binary_Search.java
@@ -1,8 +1,23 @@
+import java.util.Random;
 
 public class Binary_Search {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Random randomNumber = new Random();
+		
+		int counter = 1; 
+		
+				for (int i = 0; i <= 10; i++) {
+			
+		
+					int n = randomNumber.nextInt(101); //selecting one random number from 0-100 range
+		
+							System.out.println("Number " + counter + " is " + n);
+		
+							counter++; // counter iterated 
+		}
+		
 
 	}
 
